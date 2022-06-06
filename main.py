f4684700f54cc177afd7266c7131ff4370ce7da5
MaxVK03 <maxvonklemperer@gmail.com>
2022-06-06 13:55:58 +0200

    Program init

--- main.py
+++ main.py
@@ -1,1 +1,88 @@
+from aifc import Error
+from datetime import time
+import time
+# import NewsGet
+import mysql.connector
+import QandARet
+import GoogleImage
+import QandARet
+# import TextFromWebGet
+import YouTube
+
 print("Hello world")
+
+# NewsTex = NewsGet.getNews.texx
+# cluster = YouTube.getYout.cluster
+# print(cluster[0])
+# vidURLS = YouTube.getYout.URLs
+# ImgURLS = GoogleImage.getGoogImage.FinImg
+
+oriQ = QandARet.getQandA.ConfirmedOrig
+ans = QandARet.getQandA.ConfirmedAns
+que = QandARet.getQandA.ConfirmedQuest
+
+ClusterIDOfSubject = 8
+# TextFromSites = TextFromWebGet.webCont.TextFromsites
+# print(TextFromSites)
+
+try:
+    connection = mysql.connector.connect(host='34.203.153.217',
+                                         database='RichSnipPageGeneration',
+                                         user='root',
+                                         password='?w$D<U):;(f]3n3[')
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+        for i in range(0, len(ans)):
+            time.sleep(0.5)
+            cursor = connection.cursor()
+            sql_insert_query = """INSERT INTO qandout(ClusterID, OrigQuestion, Question, Answer)
+            VALUES (%s,%s,%s,%s)"""
+            insert_tuple_1 = (ClusterIDOfSubject, oriQ[i], que[i], ans[i])
+            cursor.execute(sql_insert_query, insert_tuple_1)
+            connection.commit()
+        '''
+        for i in range(0, len(vidURLS)):
+            time.sleep(0.5)
+            cursor = connection.cursor()
+            sql_insert_query = """INSERT INTO youtubevids(ClusterID, URLOfVid)
+                        VALUES (%s,%s)"""
+            insert_tuple_1 = (int(ClusterIDOfSubject), str(vidURLS[i]))
+            cursor.execute(sql_insert_query, insert_tuple_1)
+            connection.commit()
+
+        for i in range(0, len(ImgURLS)):
+            time.sleep(0.5)
+            cursor = connection.cursor()
+            sql_insert_query = """INSERT INTO images(ClusterID, URLOfImage)
+                               VALUES (%s,%s)"""
+            insert_tuple_1 = (int(ClusterIDOfSubject), str(ImgURLS[i]))
+            cursor.execute(sql_insert_query, insert_tuple_1)
+            connection.commit()
+
+
+        cursor = connection.cursor()
+        sql_insert_query = """INSERT INTO pagetext(ClusterID, Text)
+                                       VALUES (%s,%s)"""
+        insert_tuple_1 = (ClusterIDOfSubject, TextFromSites)
+        cursor.execute(sql_insert_query, insert_tuple_1)
+        connection.commit()
+
+        cursor = connection.cursor()
+        sql_insert_query = """INSERT INTO newstext(ClusterID, NewsText)
+                                       VALUES (%s,%s)"""
+        insert_tuple_1 = (ClusterIDOfSubject, NewsTex)
+        cursor.execute(sql_insert_query, insert_tuple_1)
+        connection.commit()
+        '''
+
+        print("Table connected succsesfully")
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
